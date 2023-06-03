75f260639bf043ff6a41e6ab5bdce95d1e292216
Pablo Andres Onofre Riascos <108633592+PabloandresDev@users.noreply.github.com>
2023-06-02 23:58:53 -0500

    Update Ejercicio21.java

--- EjerciciosAprendizaje/Ejercicio21.java
+++ EjerciciosAprendizaje/Ejercicio21.java
@@ -26,7 +26,8 @@ public class Ejercicio21 {
                 miniMatriz[i][j] = matriz[i + 4][j + 4];
             }
         }
-
+        //verificar haciendo modificaciones
+        //miniMatriz[1][1]=77;
         mostrarMatriz(matriz);
         mostrarMatriz(miniMatriz);
         verificarMatrizContenida(matriz, miniMatriz);
@@ -35,15 +36,19 @@ public class Ejercicio21 {
     public static void verificarMatrizContenida(int[][] matriz, int[][] miniMatriz) {
         boolean esContenida = false;
 
-        for (int i = 0; i < 10; ++i) {
+         for (int i = 0; i < 10; ++i) {
             for (int j = 0; j < 10; ++j) {
-                if (matriz[i][j] == miniMatriz[0][0] && matriz[i][j + 1] == miniMatriz[0][1] && matriz[i][j + 2] == miniMatriz[0][2] && matriz[i + 1][j] == miniMatriz[1][0] && matriz[i + 1][j + 1] == miniMatriz[1][1] && matriz[i + 1][j + 2] == miniMatriz[1][2] && matriz[i + 2][j] == miniMatriz[2][0] && matriz[i + 2][j + 1] == miniMatriz[2][1] && matriz[i + 2][j + 2] == miniMatriz[2][2]) {
-                    esContenida = true;
-                    System.out.println("La mini matriz si esta contenida y esta en las siguientes posiciones: ");
-                    System.out.println("" + i + "-" + j + "," + i + "-" + (j + 1) + "," + i + "-" + (j + 2));
-                    System.out.println(i + 1 + "-" + j + "," + (i + 1) + "-" + (j + 1) + "," + (i + 1) + "-" + (j + 2));
-                    System.out.println(i + 2 + "-" + j + "," + (i + 2) + "-" + (j + 1) + "," + (i + 2) + "-" + (j + 2));
-                    return;
+                if (matriz[i][j] == miniMatriz[0][0] && matriz[i][j + 1] == miniMatriz[0][1] && matriz[i][j + 2] == miniMatriz[0][2]) {
+                    if (matriz[i + 1][j] == miniMatriz[1][0] && matriz[i + 1][j + 1] == miniMatriz[1][1] && matriz[i + 1][j + 2] == miniMatriz[1][2]) {
+                        if (matriz[i + 2][j] == miniMatriz[2][0] && matriz[i + 2][j + 1] == miniMatriz[2][1] && matriz[i + 2][j + 2] == miniMatriz[2][2]) {
+                            esContenida = true;
+                            System.out.println("La mini matriz si esta contenida y esta en las siguientes posiciones: ");
+                            System.out.println("" + i + "-" + j + "," + i + "-" + (j + 1) + "," + i + "-" + (j + 2));
+                            System.out.println(i + 1 + "-" + j + "," + (i + 1) + "-" + (j + 1) + "," + (i + 1) + "-" + (j + 2));
+                            System.out.println(i + 2 + "-" + j + "," + (i + 2) + "-" + (j + 1) + "," + (i + 2) + "-" + (j + 2));
+                            return;
+                        }
+                    }
                 }
             }
         }
